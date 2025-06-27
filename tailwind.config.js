f52e58f4c41246309b2a03895e5b3b7dfe18a9a6
Breno-Goulart <breno_goulart98@outlook.com>
2025-06-27 01:38:22 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/*.js", // Inclui todos os arquivos .js dentro da pasta js/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
